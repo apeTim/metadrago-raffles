837d5fb0c370b1585810dbb3b32273524da87bd3
fei <fei@dog.com>
2022-06-02 13:16:43 +0700

    post a pubkey for pending

--- components/OpenRaffle/OpenRaffle.utils.ts
+++ components/OpenRaffle/OpenRaffle.utils.ts
@@ -1,13 +1,13 @@
-import * as splToken from '@solana/spl-token';
-import * as web3 from '@solana/web3.js';
-import * as anchor from '@project-serum/anchor';
-import { WalletContextState } from '@solana/wallet-adapter-react';
+import * as splToken from "@solana/spl-token";
+import * as web3 from "@solana/web3.js";
+import * as anchor from "@project-serum/anchor";
+import { WalletContextState } from "@solana/wallet-adapter-react";
 
 const rpcHost = process.env.NEXT_PUBLIC_RPC_HOST_URL!;
-console.log(rpcHost, 'rpcHost');
+console.log(rpcHost, "rpcHost");
 
 const connection = new anchor.web3.Connection(rpcHost, {
-  commitment: 'confirmed',
+  commitment: "confirmed",
 });
 
 //TRANSFER_ADDRESS
@@ -19,9 +19,9 @@ export async function buyRaffle(
   price: number
 ) {
   if (!wallet.publicKey) {
-    throw new Error('wallet not connected');
+    throw new Error("wallet not connected");
   }
-  if (amount === 0) throw new Error('Need to specify higher ticket amount');
+  if (amount === 0) throw new Error("Need to specify higher ticket amount");
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/alive/${id}`
@@ -29,9 +29,7 @@ export async function buyRaffle(
   const jsonResponse = await response.json();
 
   if (jsonResponse.alive === true) {
-    const mint = new web3.PublicKey(
-      process.env.NEXT_PUBLIC_SPL_TOKEN
-    );
+    const mint = new web3.PublicKey(process.env.NEXT_PUBLIC_SPL_TOKEN);
 
     const mintTokenAccount = await splToken.Token.getAssociatedTokenAddress(
       splToken.ASSOCIATED_TOKEN_PROGRAM_ID,
@@ -44,8 +42,7 @@ export async function buyRaffle(
       splToken.TOKEN_PROGRAM_ID,
       mint,
       new web3.PublicKey(process.env.NEXT_PUBLIC_SPL_TRANSFER_ADDRESS)
-    )
-
+    );
 
     const transferItx = new web3.Transaction().add(
       splToken.Token.createTransferInstruction(
@@ -66,23 +63,24 @@ export async function buyRaffle(
     const signed = await wallet.signTransaction(transferItx);
     const signature = await connection.sendRawTransaction(signed.serialize());
     const resp1 = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ticket`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         raffleId: id,
         txHash: signature,
+        wallet: wallet.publicKey.toBase58(),
       }),
     });
     if (resp1.status !== 200) {
-      throw new Error('error on raffle server');
+      throw new Error("error on raffle server");
     }
     const json1 = await resp1.json();
 
     return json1;
   } else {
-    throw new Error('couldnt connect to raffle server');
+    throw new Error("couldnt connect to raffle server");
   }
 }
